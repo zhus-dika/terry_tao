28c461658cc857d0d537aee24a39f0f156e5c90c
zhus-dika <zhus.dinara@gmail.com>
2020-02-10 02:02:09 +0600

    add quote section in admin panel

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -10,7 +10,7 @@
           .header__quit-link
             a.quit-link(href="#") Quit
     nav.nav 
-      .container
+      .container.nav__container
         - var navList = ["About", "Papers", "Quotes"]
           ul.nav__list
             each item in navList
@@ -128,18 +128,61 @@
                       .button__name Remove
                       .current-paper__wrapper
                         button.current-paper__remove
-
-
+    .quotes
+      .container.quotes__container
+        .add__quote-wrapper
+          .form__title New quote
+          .quote__container
+            .add__quote
+              .quote__author-img 
+              .new-quote__text 
+                button.add__photo(name='add__photo') Add photo
+            .quote-form__wrapper
+              form.quote-form
+                .form__rows
+                  .form__row
+                    label.form__block(for='')
+                      .form-label Author's name
+                      input.form-input__quote(name='author-name' type='text' placeholder="") 
+                    label.form__block(for='')
+                      .form-label Author's occupation
+                      input.form-input__quote(name='author-occ' type='text' placeholder="") 
+                  .form__row
+                    label.form__block(for='')
+                      .form-label Quote
+                      textarea.form-input__quote(name='quote' type='text' placeholder="" rows="3" cols = "43")
+                .form__buttons
+                  button.form__button-cancel Cancel
+                  button.form__button-save Save
+        .quotes__list
+          .new-quote
+            .add-quote__wrapper-button
+              button.add-quote(name="add-quote")
+            .quote__title Add quote
+          .current-quote
+            .current-quote__content
+              .quote__author-content
+                .quote__author-photo
+                  img(src="../images/content/timothy.jpeg" class="quote__author-img")
+                .quote__author-text
+                  .quote__author-name Timothy Gowers
+                  .quote__author-occupation British mathematician
+              .quote__content-text Tao's mathematical knowledge has an extraordinary combination of breadth and depth: he can write confidently and authoritatively on topics as diverse as partial differential equations, analytic number theory, the geometry of 3-manifolds, nonstandard analysis, group theory, model theory, quantum mechanics, probability, ergodic theory, combinatorics, harmonic analysis, image processing, functional analysis, and many others. Some of these are areas to which he has made fundamental contributions.
+              .current-paper__buttons
+                .current-paper__edit-content
+                  .button__name Edit
+                  .current-paper__wrapper
+                    button.current-paper__edit
+                .current-paper__remove-content
+                  .button__name Remove
+                  .current-paper__wrapper
+                    button.current-paper__remove
+          
+                 
 
 
           
-
-
-
-
-
-
-
+          
 
 </template>
 
