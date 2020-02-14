d53cd800cc5438c8bb85ecaaa305470a63c48cab
zhus-dika <zhus.dinara@gmail.com>
2020-02-14 12:28:09 +0600

    1st ed: add auth window in admin panel

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -1,5 +1,21 @@
 <template lang="pug">
   section.about
+    .authorization
+      .auth__wrapper
+        .auth__window
+          h2.auth__header Authorization
+          button.close-window
+          form.auth__form(action='')
+            label.form__block(for='') 
+              .form-label Login
+              .form-input__wrapper-login
+                input.auth__form-input(name='auth' type='login' placeholder="terry_tao" required)
+            label.form__block(for='') 
+              .form-label Password
+              .form-input__wrapper-password
+                input.auth__form-input(name='password' type='password' placeholder="*******" required)
+            .auth__form-button__wrapper
+              input.auth__form-button(type='submit') 
     header.header
       .container.header__container
           .header__content
